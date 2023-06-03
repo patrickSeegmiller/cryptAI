1d62d968a5d83b578afcd71ff52b302505c3fdae
Patrick Seegmiller <patrick.seegmiller@gmail.com>
2023-06-03 11:55:41 -0600

    Update rational_number_operations.py

--- rational_number_operations.py
+++ rational_number_operations.py
@@ -4,30 +4,32 @@ def continued_fraction_expansion(num: int, denom: int) -> list[int]:
     cf_expansion takes the numerator and denominator of a positive rational number as parameters and returns its
     continued fraction expansion as a list.
 
-    Parameters
-    ----------
-    num : int
-        num should be the numerator of the rational number to be converted to a continued fraction
+    Args:
+        num (int): The numerator of the rational number to be converted to a continued fraction.
+        denom (int): The denominator of the rational number to be converted to a continued fraction.
 
-    denom : int
-        denom should be the denominator of the rational number to be converted to a continued fraction
+    Returns:
+        list[int]: The continued fraction expansion of the rational number num/denom.
 
-    Returns
-    -------
-    list of ints  
-        The continued fraction expansion of the rational number num/denom
     """
 
-    i_part = num // denom
-    r = num % denom
+    # We first check that num and denom are positive integers.
+    if not isinstance(num, int) or not isinstance(denom, int) or num <= 0 or denom <= 0:
+        raise ValueError("num and denom should be positive integers.")
     
-    result = [i_part]
-    while r != 0:
-        num, denom = denom, r
-        i_part = num // denom
-        r = num % denom
-        result.append(i_part)
+    # Next, we initialize the integer part, remainder, and result list.
+    integer_part = num // denom
+    remainder = num % denom
+    result = [integer_part]
 
+    # We now compute the continued fraction expansion, appending each integer part to the result list.
+    while remainder != 0:
+        num, denom = denom, remainder
+        integer_part = num // denom
+        remainder = num % denom
+        result.append(integer_part)
+
+    # We return the continued fraction expansion.
     return result
 
 def get_continued_fraction_convergents(continued_fraction: list[int]) -> list[tuple[int, int]]:
@@ -35,16 +37,17 @@ def get_continued_fraction_convergents(continued_fraction: list[int]) -> list[tu
     get_cf_convergents takes a continued fraction expansion as a list and returns a list of tuples containing the
     numerator and denominator of each convergent.
 
-    Parameters
-    ----------
-    continued_fraction : list of ints 
-        continued_fraction should be a list containing the continued fraction expansion of a rational number
+    Args:
+        continued_fraction (list[int]): The continued fraction expansion of a rational number as a list.
 
-    Returns
-    -------
-    list of tuples
-        Each tuple contains the numerator and denominator of a convergent of the continued fraction
+    Returns:
+        list[tuple[int, int]]: A list of tuples containing the numerator and denominator of each convergent of the
+        continued fraction.
     
+    Raises:
+        TypeError: If continued_fraction is not a list of integers. 
+        ValueError: If continued_fraction is empty.
+
     """
 
     # We first check that continued_fraction is a list of integers.
